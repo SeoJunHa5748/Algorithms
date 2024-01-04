5c6215c7d1970cc9b285fd237e9517fd110e58af
SEO <wnsgk5748@naver.com>
2024-01-04 12:23:19 +0900

    2024-01-04 11 : 40 ~ 12 : 25 baekjoon CandyGame unfinished

--- Programmers/src/basic/TheLastTwoElements.java
+++ Programmers/src/basic/TheLastTwoElements.java
@@ -5,7 +5,7 @@ public class TheLastTwoElements {
 	
 	public int[] solution(int[] num_list) {
         int[] answer = {};
-        if(num_list[num_list.length-1] )
+        //if(num_list[num_list.length-1] )
         return answer;
     }
 }
